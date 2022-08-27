e52072c505883fbc40a728f9eb7c436f1e4ebbc7
Sahan Perera <sahanvimukthi00@gmail.com>
2022-08-27 18:29:56 +0530

    destination model added

---
+++ Project/backend/models/destination.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const destinationSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    
