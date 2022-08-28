78fa7ee9b66ecd2cf65fa01229d8eb72f10d940f
Sahan Perera <sahanvimukthi00@gmail.com>
2022-08-28 17:22:29 +0530

    fixed bug

--- Project/backend/controllers/destination.js
+++ Project/backend/controllers/destination.js
@@ -34,7 +34,7 @@ export const updateDestination = async (req, res) => {
     const id = req.params.id;
     const update = req.body;
     try {
-        const destination = await Destination.findByIdAndUpdate(id, update);
+        await Destination.findByIdAndUpdate(id, update);
         res.status(200).send({status: "Destination updated"});
     } catch (error) {
         res.status(404).json({ message: error });
@@ -44,7 +44,7 @@ export const updateDestination = async (req, res) => {
 export const deleteDestination = async (req, res) => {
     const id = req.params.id;
     try {
-        const destination = await Destination.findByIdAndDelete(id);
+        await Destination.findByIdAndDelete(id);
         res.status(200).send({status: "Destination deleted"});
     }catch{
         res.status(404).json({ message: error });
