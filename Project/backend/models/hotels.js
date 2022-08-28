9f7d28a68a9e5270fd56b6763e711f9219e98cd4
GRieZzLEO <chamodhleo1@gmail.com>
2022-08-28 15:08:53 +0530

    Hotel Model Created

---
+++ Project/backend/models/hotels.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const Schema =mongoose.Schema;
+
+const hotelSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    location:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    stars:{
+        type:Number,
+        required:true
+    },
+    facilities:{
+        type:[String],
+        required: true
+    },
+    images:{
+        type:[String],
+        required: true
+    }
+});
+
+const Hotel = mongoose.model("hotel",hotelSchema); /*Document to be directed in the mongoDB*/
+
+export default Hotel;
