171a3c5eda6a9b2d73d238a26fa2b9081f52e5e7
Sahan Perera <sahanvimukthi00@gmail.com>
2022-08-28 17:31:47 +0530

    fixed a bug

--- Project/backend/routes/desTicket.js
+++ Project/backend/routes/desTicket.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/", getAllDesTickets);
 router.get("/:id", getDesTicket);
-router.get("/:userId", getUserDesTickets);
+router.get("/uesr/:userId", getUserDesTickets);
 router.post("/create", createDesTicket);
 router.put("/update/:id", updateDesTicket);
 router.delete("/delete/:id", deleteDesTicket);
