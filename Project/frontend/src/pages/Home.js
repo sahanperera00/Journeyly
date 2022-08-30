f96224ecc8f6ad1f73d35d118f909000fe2e6ba3
Sahan Perera <sahanvimukthi00@gmail.com>
2022-08-30 21:08:24 +0530

    implemented home page

---
+++ Project/frontend/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from "../components/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            <NavBar />
+            <h1 className='text-center'>Home</h1>
+        </div>
+    );
+}
+
+export default Home;
