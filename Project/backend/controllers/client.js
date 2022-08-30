a8620998917b1f784a58d8d4d9bb9d65c5f97a2d
IT21004636 <it21004636@my.sliit.lk>
2022-08-30 11:20:29 +0530

    bug fixed

--- Project/backend/controllers/client.js
+++ Project/backend/controllers/client.js
@@ -13,7 +13,7 @@ export const getAllClients = async (req, res) => {
 export const getClient = async (req, res) => {
     const id = req.params.id;
     try {
-        const destination = await Client.findById(id);
+        const client = await Client.findById(id);
         res.status(200).json(client);
     } catch (error) {
         res.status(404).json({ message: error });
