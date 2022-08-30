0fc0aa60997955e3cd70dd1c5040b6ca803f82fa
Sahan Perera <sahanvimukthi00@gmail.com>
2022-08-30 19:40:30 +0530

    Routes folder added

--- Project/frontend/src/App.js
+++ Project/frontend/src/App.js
@@ -1,20 +1,11 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
-// import DestinationForm from "./components/DestinationForm";
-import EditorDashboard from "./pages/EditorDashboard";
+import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    <Router>
       <div>
-        <Navbar />
-        <Routes>
-          {/* <Route path="/editor" element={<DestinationForm />} /> */}
-          <Route path="/editor" element={<EditorDashboard />} />
-        </Routes>
+        <AppRoutes />
       </div>
-    </Router>
   );
 }
 
