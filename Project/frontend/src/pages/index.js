77f10e6590ea3d75261c6517223459016523ced0
Sahan Perera <sahanvimukthi00@gmail.com>
2022-08-31 15:49:12 +0530

    index.js of pages created

---
+++ Project/frontend/src/pages/index.js
@@ -0,0 +1,23 @@
+import Home  from '../pages/Home';
+import EditorDashboard  from '../pages/EditorDashboard';
+import NavBar from "../components/Navbar";
+import Flights from "../pages/Flights";
+import Hotels from "../pages/Hotels";
+import Attractions from "../pages/Attractions";
+import Taxis from "../pages/Taxis";
+import Login from "../pages/Login";
+import EditorNavBar from "../components/EditorNavbar";
+import DestinationForm from "../components/DestinationForm";
+
+export {
+    Home,
+    EditorDashboard,
+    NavBar,
+    Flights,
+    Hotels,
+    Attractions,
+    Taxis,
+    Login,
+    EditorNavBar,
+    DestinationForm
+};
