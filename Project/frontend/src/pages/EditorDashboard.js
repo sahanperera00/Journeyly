a404e21117cfb70643e909426ce3e4e2d9fa5b37
Sahan Perera <sahanvimukthi00@gmail.com>
2022-08-31 11:26:07 +0530

    removed navbar from dashboard

--- Project/frontend/src/pages/EditorDashboard.js
+++ Project/frontend/src/pages/EditorDashboard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import NavBar from "../components/Navbar";
 import EditorNavBar from "../components/EditorNavbar";
 import DestinationForm from "../components/DestinationForm";
 import { Route, Routes } from 'react-router-dom';
@@ -7,9 +6,8 @@ import { Route, Routes } from 'react-router-dom';
 function EditorDashboard() {
   return (
     <div>
-      <NavBar />
-      <h1 className='text-center'>Editor Dashboard</h1>
       <EditorNavBar />
+      <h1 className='text-center'>Editor Dashboard</h1>
       <Routes>
         <Route path="/desform" element={<DestinationForm />} />
       </Routes>
