aff394e85ac65ea7fcc1af590d928668f0e681b8
Sahan Perera <sahanvimukthi00@gmail.com>
2022-08-31 15:48:17 +0530

    editor navbar updated

--- Project/frontend/src/components/EditorNavbar.js
+++ Project/frontend/src/components/EditorNavbar.js
@@ -2,12 +2,17 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import {LinkContainer} from 'react-router-bootstrap';
+import { Link } from "react-router-dom";
 
 function EditorNavbar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Editor Dashboard</Navbar.Brand>
+        <LinkContainer to="/editorDash">
+          <Navbar.Brand>Editor Dashboard</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -24,7 +29,9 @@ function EditorNavbar() {
               <NavDropdown.Item href="#action/3.4">Client View</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Attractions" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#desForm">Create</NavDropdown.Item>
+              <LinkContainer to="/addAttractionsForm">
+                <NavDropdown.Item>Create</NavDropdown.Item>
+              </LinkContainer>
               <NavDropdown.Item href="#action/3.3">Update & Delete</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">Client View</NavDropdown.Item>
@@ -36,6 +43,9 @@ function EditorNavbar() {
               <NavDropdown.Item href="#action/3.4">Client View</NavDropdown.Item>
             </NavDropdown>
           </Nav>
+          <Link to={"/"}>
+                <Button variant="outline-dark ms-2">Logout</Button>
+          </Link>
         </Navbar.Collapse>
       </Container>
     </Navbar>
