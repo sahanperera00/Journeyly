c4d8092bde625476f5ed1155d2a7e060fa6cc1f9
Sahan Perera <sahanvimukthi00@gmail.com>
2022-09-01 02:22:56 +0530

    outlet added

--- Project/frontend/src/pages/EditorDashboard.js
+++ Project/frontend/src/pages/EditorDashboard.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import { Outlet } from 'react-router-dom';
+import EditorNavbar from '../components/EditorNavbar';
 
 function EditorDashboard() {
   return (
     <div>
+      <EditorNavbar />
       <h1 className='text-center'>Dashboard</h1>
+      <Outlet />
     </div>
   );
 }
