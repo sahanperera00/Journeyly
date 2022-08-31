2e3d582ed1261b1f330d836931658c23cbfa87de
Sahan Perera <sahanvimukthi00@gmail.com>
2022-09-01 03:22:46 +0530

    index routes added

--- Project/frontend/src/pages/EditorDashboard.js
+++ Project/frontend/src/pages/EditorDashboard.js
@@ -1,12 +1,7 @@
-import { Outlet } from 'react-router-dom';
-import EditorNavbar from '../components/EditorNavbar';
-
 function EditorDashboard() {
   return (
     <div>
-      <EditorNavbar />
       <h1 className='text-center'>Dashboard</h1>
-      <Outlet />
     </div>
   );
 }
