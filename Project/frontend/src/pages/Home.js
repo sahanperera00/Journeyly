d9cce5dc218e706556b52b588bcbad0694b6e296
Sahan Perera <sahanvimukthi00@gmail.com>
2022-08-31 10:36:10 +0530

    removed navbar from home page

--- Project/frontend/src/pages/Home.js
+++ Project/frontend/src/pages/Home.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import NavBar from "../components/Navbar";
 
 const Home = () => {
     return (
         <div>
-            <NavBar />
+            {/* <NavBar /> */}
             <h1 className='text-center'>Home</h1>
         </div>
     );
