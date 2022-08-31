ffd1b79061d614995ecf286ee5877e8b8e23acba
Sahan Perera <sahanvimukthi00@gmail.com>
2022-08-31 10:36:26 +0530

    routes updated

--- Project/frontend/src/routes/AppRoutes.js
+++ Project/frontend/src/routes/AppRoutes.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home  from '../pages/Home';
 import EditorDashboard  from '../pages/EditorDashboard';
 import DestinationForm  from '../components/DestinationForm';
+import NavBar from "../components/Navbar";
 
 function AppRoutes() {
     return (
         <Router>
+            <NavBar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/editorDash" element={<EditorDashboard />} />
