e35798f3b5d82124e31d90b7bf26d1e43e96997f
Sahan Perera <sahanvimukthi00@gmail.com>
2022-09-04 20:16:14 +0530

    redo changes

--- Project/frontend/src/firebase.js
+++ Project/frontend/src/firebase.js
@@ -1,4 +1,4 @@
-import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
@@ -10,5 +10,5 @@ const firebaseConfig = {
   appId: "1:503942966112:web:4e05e59d6c41db2e674c05"
 };
 
-const app = firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
