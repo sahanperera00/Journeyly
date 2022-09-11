01ac44adaaa080f6f30eeed8fa07e8d84143d26c
Sahan Perera <sahanvimukthi00@gmail.com>
2022-09-11 17:39:36 +0530

    styles added to the home page

--- Project/frontend/src/pages/Home.js
+++ Project/frontend/src/pages/Home.js
@@ -1,7 +1,16 @@
+import '../styles/sahan/Home.css';
+import logo from '../images/Journeyly&Slogan-color.png';
+
 function Home() {
     return (
-        <div>
-            <h1 className='text-center'>Home</h1>
+        <div className='homeCont'>
+            <div className='heroCont'>
+                <div className='heroText'>
+                    <img src={logo} alt='heroimg' />
+                    <h1>Travel with us</h1>
+                    <p>Travel with us to the most beautiful places in Sri Lanka</p>
+                </div>
+            </div>
         </div>
     );
 }
