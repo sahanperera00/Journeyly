ac9fc67d9ccdd67fdf4b29ee2d2fc47bb0f1a2e1
IT21008382 <87684152+IT21008382@users.noreply.github.com>
2022-09-13 23:36:22 +0530

    Update index.js

--- Project/backend/index.js
+++ Project/backend/index.js
@@ -10,6 +10,8 @@ import hotelResRouter from "./routes/hotelReservation.js";
 import flightRouter from "./routes/flights.js";
 import packageRouter from "./routes/packages.js";
 import clientRouter from "./routes/client.js";
+import rentalRoutes from './routes/rental.js'
+import vehicleRoutes from './routes/vehicles.js'
 
 const app = express();
 const PORT = process.env.PORT || 8070;
@@ -23,6 +25,8 @@ app.use("/hotelRes",hotelResRouter);
 app.use("/flights",flightRouter);
 app.use("/packages",packageRouter);
 app.use("/client",clientRouter);
+app.use('/rental', rentalRoutes);
+app.use('/vehicle', vehicleRoutes);
 
 const URL = process.env.MONGODB_URL;
 mongoose.connect(URL);
