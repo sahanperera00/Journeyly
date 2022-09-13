7b3e16f59a0571671a02b5ad823d2afa24028385
IT21008382 <87684152+IT21008382@users.noreply.github.com>
2022-09-13 23:29:46 +0530

    updated vehicle and rental controllers

--- Project/backend/controllers/rental.js
+++ Project/backend/controllers/rental.js
@@ -1,28 +1,67 @@
-import PostMessage from "../models/rental.js";
+import rental from "../models/rental.js";
 
-export const getRental =  async (req, res) => {
+export const getAllRental =  async (req, res) => {
     try {
-        const rentals = await rentals.find();
+        const rentals = await rental.find();
 
         res.status(200).json(rentals);
 
     } catch (error) {
-        res.status(404).jason( {message: error.message} );
+        res.status(404).json( {message: error.message} );
+    }
+}
+
+export const getRental = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const Rental = await rental.findById(id);
+        res.status(200).json(rental);
+    } catch (error) {
+        res.status(404).json({ message: error });
     }
 }
 
 export const createRental =  async (req, res) => {
-    const rental = req.body;
+    const Rental = req.body;
 
-    const newRental = new rental(rental);
+    const newRental = new rental(Rental);
 
     try {
-        await newPost.save();
+        await newRental.save();
 
         res.status(201).json(newRental);
 
 
     } catch (error) {
         res.status(409).json(  {message: error.message} );
+    }
+}
+
+export const updateRental = async (req, res) => {
+
+    const id = req.params.id;
+
+    const updateRentals = req.body;
+
+    try {
+        await rental.findByIdAndUpdate(id, updateRentals);
+
+        res.status(200).send({status: "Rental updated"});
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const deleteRental = async (req, res) => {
+
+    const id = req.params.id;
+
+    try {
+        await rental.findByIdAndDelete(id);
+
+        res.status(200).send({status: "Rental deleted"});
+
+    }catch{
+        res.status(404).json({ message: error.message });
     }
 }
