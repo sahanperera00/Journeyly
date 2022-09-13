8852b85f25d3f1a5516778aa2136c79ede899feb
SuDelk <sudul.fernando@gmail.com>
2022-09-13 13:19:31 +0530

    flight

--- Project/frontend/src/pages/Flights.js
+++ Project/frontend/src/pages/Flights.js
@@ -24,20 +24,20 @@ function Flights() {
       <div className='container d-flex flex-wrap' style={{ width: '80%'}}>
         {flights.map((data) => {
           return (
-            <Card style={{ width: '19rem', margin: '1rem', padding: '1rem'}}>
+            <Card style={{ width: '30rem', margin: '1rem', padding: '1rem'}}>
               <Card.Body>
                 <Card.Title>{data.name}</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">{data.flightId}</Card.Subtitle>
                 <Card.Text>
-                Leaving Airport: {data.startAirport}<br/>
-                  Departure Date: {data.departureDate}<br/>
-                  Departure Time: {data.departureTime}<br/>
-                  Destination Airport: {data.destinationAirport}<br/>
-                  Arrival Date: {data.arrivalDate}<br/>
-                  Arrival Time: {data.arrivalTime}<br/>
-                  Economy Class Ticket Price: {data.economyClass}<br/>
-                  Business Class Ticket Price: {data.businessClass}<br/>
-                  Image: {data.images}<br/>
+                 <b> Leaving Airport            : </b>{data.startAirport}<br/>
+                 <b> Departure Date             : </b>{data.departureDate}<br/>
+                 <b> Departure Time             : </b>{data.departureTime}<br/>
+                 <b> Destination Airport        : </b>{data.destinationAirport}<br/>
+                 <b> Arrival Date               : </b>{data.arrivalDate}<br/>
+                 <b> Arrival Time               : </b>{data.arrivalTime}<br/>
+                 <b> Economy Class Ticket Price : </b>{data.economyClass}<br/>
+                 <b> Business Class Ticket Price: </b>{data.businessClass}<br/>
+                 <b> Image                      : </b>{data.images}<br/>
                 </Card.Text>
               </Card.Body>
             </Card>
