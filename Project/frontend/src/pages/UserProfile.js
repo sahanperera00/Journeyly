6c9ced3e52b2052e8bf11f13006901133ea8aef6
IT21004636 <it21004636@my.sliit.lk>
2022-09-22 17:22:55 +0530

    created user profile

---
+++ Project/frontend/src/pages/UserProfile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/nash/UserProfile.css'
+
+function UserProfile() {
+  return (
+    <div className='userprofilecontainer'>
+        
+        <h1>User Profile</h1>
+    
+    </div>
+  )
+}
+
+export default UserProfile
