2dc43bf78f75ff97ea4ae89d320da76333978e8c
IT21004636 <it21004636@my.sliit.lk>
2022-09-22 18:55:37 +0530

    imported login routes

--- Project/backend/routes/client.js
+++ Project/backend/routes/client.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getAllClients, getClient, createClient, updateClient, deleteClient } from "../controllers/client.js";
+import { getAllClients, getClient, createClient, updateClient, deleteClient, loginClient } from "../controllers/client.js";
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.get("/:id", getClient);
 router.post("/create", createClient);
 router.put("/update/:id", updateClient);
 router.delete("/delete/:id", deleteClient);
+router.post("/login", loginClient );
 
 export default router;
