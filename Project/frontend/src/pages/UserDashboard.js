3ffda143f6de78193ba11906ec20ac468ab22807
IT21004636 <it21004636@my.sliit.lk>
2022-09-22 22:24:46 +0530

    added logout button

--- Project/frontend/src/pages/UserDashboard.js
+++ Project/frontend/src/pages/UserDashboard.js
@@ -1,7 +1,8 @@
 
 import { Outlet } from 'react-router-dom'
 import UserDashboardSidebar from './UserDashboardSidebar'
-// import '../styles/nash/UserDashboard.css'
+import '../styles/nash/UserDashboard.css'
+import { Link } from "react-router-dom";
 
 function UserDashboard() {
   return (
@@ -9,6 +10,10 @@ function UserDashboard() {
 
         <UserDashboardSidebar/>
         <Outlet/>
+       
+        <Link to={"/"}>
+                <button className='btnlogout'>Logout</button>
+        </Link>
         
     </div>
   )
