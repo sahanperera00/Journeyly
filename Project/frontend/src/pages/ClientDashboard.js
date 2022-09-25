701f4afa89fe500ad26602e9da63821aa950b6f2
IT21004636 <it21004636@my.sliit.lk>
2022-09-25 23:46:02 +0530

    implemented client dashboard

---
+++ Project/frontend/src/pages/ClientDashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ClientDashSidebar from '../components/ClientDashSidebar'
+
+function ClientDashboard() {
+  return (
+    
+    <div>
+        <ClientDashSidebar/>
+      
+        
+        </div>
+  )
+}
+
+export default ClientDashboard
