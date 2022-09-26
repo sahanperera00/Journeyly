0a9a606ea90d9e7833746c597722aa6d0e6f65af
IT21008382 <87684152+IT21008382@users.noreply.github.com>
2022-09-26 13:41:45 +0530

    Update RentalPreview.js

--- Project/frontend/src/pages/RentalPreview.js
+++ Project/frontend/src/pages/RentalPreview.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import '../styles/Madusha/taxiPreview.css';
+import '../styles/Madusha/TaxiPreview.css';
 import { Link } from 'react-router-dom';
 
 function RentalPreview() {
