d61cd0125475c9ad07fd282a2fcede9058c334dc
IT21008382 <it21008382@my.sliit.lk>
2022-09-26 12:57:24 +0530

    slight updates

--- Project/frontend/src/pages/RentalPreview.js
+++ Project/frontend/src/pages/RentalPreview.js
@@ -1,28 +1,18 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-//import '../styles/madusha/RentalPreview.css';
-import { Link } from 'react-router-dom';
+import '../styles/madusha/RentalPreview.css';
+import rentalForm from './rentalForm';
 
-function RentalPreview() {
-
-  const [vehicleType, setVType]=useState('');
-  const [type, setType]=useState('');
-  const [driverName, setDriverName]=useState('');
-  const [fee, setFee]=useState('');
-  const [image, setImage]=useState('');
-
+function rentalPreview() {
+  const [vehicle, setVehicle] = useState([]);
 
   const {id} = useParams();
 
   const getVehicle = async () => {
     await axios.get("http://localhost:8070/vehicles/"+id)
       .then((res) => {
-        setVType(res.data.vehicleType);
-        setType(res.data.type);
-        setDriverName(res.data.driverName);
-        setFee(res.data.fee);
-        setImage(res.data.Image);
+        setVehicle(res.data);
       })
       .catch((err) => {
         alert(err);
@@ -33,23 +23,21 @@ function RentalPreview() {
 
   return (
     <>
-      <div className='rentalPreviewContainer' style={{ backgroundImage: `url(${image})` }}>
+      <div className='rentalPreviewContainer' style={{ backgroundImage: `url(${vehicle.image})` }}>
       <div className='rentalPreviewBlueDiv' />
       <div className='rentalpreviewTextContainer'>
-        <h1 className='rentalpreviewh1'>{vehicleType}</h1>
-        <h2 className='rentalpreviewh2'>{type}</h2>
-        <h3 className='rentalpreviewh3'>{driverName}</h3>
-        <h2 className='rentalpreviewp'>{fee}</h2><br />
+        <h1 className='rentalpreviewh1'>{vehicle.vehicleType}</h1>
+        <h2 className='rentalpreviewh2'>{vehicle.type}</h2>
+        <h3 className='rentalpreviewh3'>{vehicle.driverName}</h3>
+        <h2 className='rentalpreviewp'>{vehicle.fee}</h2><br />
+        <a href='#rentalform' className='reserveBtn'>Rent this vehicle</a>
       </div>
       </div>
-      <Link to={'/rentalForm'}>
-        <div>
-            <button className='hotelbtn'>Rent this Vehicle</button>
-        </div>
-      </Link>
+      <div className='rentalFormContainer'>
+        <rentalForm rental={vehicle} />
+      </div>
     </>
   )
 }
 
-
-export default RentalPreview;
+export default rentalPreview;
