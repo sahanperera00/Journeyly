1f229ffb5713f62f940686f5373cc14a439e8020
IT21004636 <it21004636@my.sliit.lk>
2022-10-22 00:38:05 +0600

    started button navigation functionality

--- Project/frontend/src/pages/Home.js
+++ Project/frontend/src/pages/Home.js
@@ -2,10 +2,16 @@ import '../styles/sahan/Home.css';
 import Carousel from 'react-bootstrap/Carousel';
 import Button from 'react-bootstrap/Button';
 import Tilt from '../components/Tilt';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from '../components/Footer';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../firebase';
+import axios from 'axios';
+import { useState } from 'react';
+import { signOut } from 'firebase/auth';
 
 function Home() {
+    const [id, setId] = useState("");
     const options = {
         reverse: true,
         speed: 1000,
@@ -13,7 +19,22 @@ function Home() {
         glare: false,
         "max-glare": 0.1,
     };
-    // checking commit
+
+    const [user, loading] = useAuthState(auth);
+
+    if (loading) return <p className='text-center mt-5'>Loading ...</p>
+
+    if (user) {
+        axios.get(`http://localhost:8070/client/getClientInfo/${user?.email}`)
+            .then((client) => {
+                setId(client.data._id);
+            }).catch((err) => {
+                console.log(err);
+                signOut(auth);
+            })
+    }
+
+
     return (
         <div className='homeCont'>
             <Carousel className='carouselCont' fade>
@@ -23,7 +44,7 @@ function Home() {
                             <img src={'https://firebasestorage.googleapis.com/v0/b/journeyly-7f164.appspot.com/o/images%2Fothers%2FJourneyly_Slogan-W.webp?alt=media&token=a652ffa4-96d9-4f20-987f-c0d8019a5283'} alt='heroimg' />
                             <h1>Travel with us</h1>
                             <p>Travel with us to the most beautiful places in Sri Lanka</p>
-                            <Link to={"/registration"}>
+                            <Link to={user?.email ? `/clientDashboard/${id}` : `/registration`}>
                                 <Button variant="outline-light">Let's get started</Button >
                             </Link>
                         </Carousel.Caption>
@@ -36,7 +57,7 @@ function Home() {
                             <img src={'https://firebasestorage.googleapis.com/v0/b/journeyly-7f164.appspot.com/o/images%2Fothers%2FJourneyly_Slogan-W.webp?alt=media&token=a652ffa4-96d9-4f20-987f-c0d8019a5283'} alt='heroimg' />
                             <h1>Travel with us</h1>
                             <p>Travel with us to the most beautiful places in Sri Lanka</p>
-                            <Link to={"/registration"}>
+                            <Link to={user?.email ? `/clientDashboard/${id}` : `/registration`}>
                                 <Button variant="outline-light">Let's get started</Button >
                             </Link>
                         </Carousel.Caption>
@@ -49,7 +70,7 @@ function Home() {
                             <img src={'https://firebasestorage.googleapis.com/v0/b/journeyly-7f164.appspot.com/o/images%2Fothers%2FJourneyly_Slogan-W.webp?alt=media&token=a652ffa4-96d9-4f20-987f-c0d8019a5283'} alt='heroimg' />
                             <h1>Travel with us</h1>
                             <p>Travel with us to the most beautiful places in Sri Lanka</p>
-                            <Link to={"/registration"}>
+                            <Link to={user?.email ? `/clientDashboard/${id}` : `/registration`}>
                                 <Button variant="outline-light">Let's get started</Button >
                             </Link>
                         </Carousel.Caption>
