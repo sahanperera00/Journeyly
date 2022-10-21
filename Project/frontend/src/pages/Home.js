c3e2b4f453c87294b5fe4d7dc95bae4da216dfff
IT21004636 <it21004636@my.sliit.lk>
2022-10-21 22:59:12 +0600

    checking commit

--- Project/frontend/src/pages/Home.js
+++ Project/frontend/src/pages/Home.js
@@ -13,7 +13,7 @@ function Home() {
         glare: false,
         "max-glare": 0.1,
     };
-
+    // checking commit
     return (
         <div className='homeCont'>
             <Carousel className='carouselCont' fade>
@@ -31,29 +31,29 @@ function Home() {
                 </Carousel.Item>
 
                 <Carousel.Item className='heroCont2' interval={2500}>
-                <Tilt options={options} className='heroText1'>
-                    <Carousel.Caption className='carousalCap'>
-                        <img src={'https://firebasestorage.googleapis.com/v0/b/journeyly-7f164.appspot.com/o/images%2Fothers%2FJourneyly_Slogan-W.webp?alt=media&token=a652ffa4-96d9-4f20-987f-c0d8019a5283'} alt='heroimg' />
-                        <h1>Travel with us</h1>
-                        <p>Travel with us to the most beautiful places in Sri Lanka</p>
-                        <Link to={"/registration"}>
-                            <Button variant="outline-light">Let's get started</Button >
-                        </Link>
-                    </Carousel.Caption>
-                </Tilt>
+                    <Tilt options={options} className='heroText1'>
+                        <Carousel.Caption className='carousalCap'>
+                            <img src={'https://firebasestorage.googleapis.com/v0/b/journeyly-7f164.appspot.com/o/images%2Fothers%2FJourneyly_Slogan-W.webp?alt=media&token=a652ffa4-96d9-4f20-987f-c0d8019a5283'} alt='heroimg' />
+                            <h1>Travel with us</h1>
+                            <p>Travel with us to the most beautiful places in Sri Lanka</p>
+                            <Link to={"/registration"}>
+                                <Button variant="outline-light">Let's get started</Button >
+                            </Link>
+                        </Carousel.Caption>
+                    </Tilt>
                 </Carousel.Item>
 
                 <Carousel.Item className='heroCont3' interval={2500}>
-                <Tilt options={options} className='heroText1'>
-                    <Carousel.Caption className='carousalCap'>
-                        <img src={'https://firebasestorage.googleapis.com/v0/b/journeyly-7f164.appspot.com/o/images%2Fothers%2FJourneyly_Slogan-W.webp?alt=media&token=a652ffa4-96d9-4f20-987f-c0d8019a5283'} alt='heroimg' />
-                        <h1>Travel with us</h1>
-                        <p>Travel with us to the most beautiful places in Sri Lanka</p>
-                        <Link to={"/registration"}>
-                            <Button variant="outline-light">Let's get started</Button >
-                        </Link>
-                    </Carousel.Caption>
-                </Tilt>
+                    <Tilt options={options} className='heroText1'>
+                        <Carousel.Caption className='carousalCap'>
+                            <img src={'https://firebasestorage.googleapis.com/v0/b/journeyly-7f164.appspot.com/o/images%2Fothers%2FJourneyly_Slogan-W.webp?alt=media&token=a652ffa4-96d9-4f20-987f-c0d8019a5283'} alt='heroimg' />
+                            <h1>Travel with us</h1>
+                            <p>Travel with us to the most beautiful places in Sri Lanka</p>
+                            <Link to={"/registration"}>
+                                <Button variant="outline-light">Let's get started</Button >
+                            </Link>
+                        </Carousel.Caption>
+                    </Tilt>
                 </Carousel.Item>
 
             </Carousel>
@@ -69,7 +69,7 @@ function Home() {
                 <button className='CeoBtn'>CEO Dashboard</button>
             </Link>
             <Link to={"/feedback/create"}>
-            <button className='FeedbackBtn'>Send A Feedback</button>
+                <button className='FeedbackBtn'>Send A Feedback</button>
             </Link>
         </div>
     );
