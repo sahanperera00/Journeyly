e831beaac66f7364dd08ba3b0d4f6d8d6a2bbc1b
Sahan Perera <sahanvimukthi00@gmail.com>
2022-11-12 01:31:59 +0530

    implemented sidebar for the flights and changed styles

--- Project/frontend/src/pages/Flights.js
+++ Project/frontend/src/pages/Flights.js
@@ -1,13 +1,14 @@
 import Card from "react-bootstrap/Card";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import "../styles/sudul/flight.css";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import "../styles/sudul/flight.css";
 import NavbarDark from "../components/NavbarDark";
 
 function Flights() {
   const [flights, setFlights] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(""); // created to catch and set the searching options
 
   const { id } = useParams();
 
@@ -24,27 +25,27 @@ function Flights() {
 
   useEffect(() => {
     getFlights();
-  }, []); //Shows changes of the page
-
-  const [searchTerm, setSearchTerm] = useState("");
+  }, []);
 
   return (
     <div className="flightMainContainer">
       <NavbarDark />
-      <div className="flightApp">
+      <div className="flightInnerContainer">
         <h1 className="flightHeader">Flights</h1>
-
         <div className="flightContainer">
-          <div className="flightSideBar">
-            <h1>Search</h1>
+          <div className="flightSearch">
+            Airline
+            <br />
+            <br />
             <input
-              className="searchSide"
+              className="flightSearchInput"
               type="text"
-              placeholder="Flight Name or Destination"
-              onChange={(event) => {
-                setSearchTerm(event.target.value);
+              placeholder="Search by airline"
+              onChange={(e) => {
+                setSearchTerm(e.target.value);
               }}
             />
+            <hr />
           </div>
           <div className="flightBodyContainer">
             {flights
@@ -53,13 +54,9 @@ function Flights() {
                   if (searchTerm == "") {
                     return data;
                   } else if (
-                    data.destinationAirport
+                    data.airline
                       .toLowerCase()
                       .includes(searchTerm.toLowerCase())
-                  ) {
-                    return data;
-                  } else if (
-                    data.name.toLowerCase().includes(searchTerm.toLowerCase())
                   ) {
                     return data;
                   }
@@ -68,45 +65,62 @@ function Flights() {
               .map((data) => {
                 return (
                   <Link to={"/flightPreview/" + data._id}>
-                    <div className="CardContainer1">
-                      <div className="ImageContainer">
+                    <div className="flightCardContainer">
+                      <div className="flightImageContainer">
                         <img
                           className="flightCardImg"
                           alt="pic"
                           src={data.imageI}
                         />
                       </div>
-                      <div className="TextContainer">
-                        <center>
-                          <h2>{data.name} </h2>
-                        </center>
-                        <center>
-                          <h7>{data.airline} </h7>
-                        </center>
-                        <p className="price">
-                          Price: Rs.{data.economyClass} upwards
-                        </p>
-                        <p className="desc">
-                          Leaving Airport: {data.startAirport}
-                        </p>
-                        <p className="desc">
-                          Departure Date: {data.departureDate} (GMT+5:30)
-                        </p>
-                        <p className="desc">
-                          Destination Airport: {data.destinationAirport}
-                        </p>
-                        <p className="desc">
-                          Arrival Date: {data.arrivalDate} (GMT+5:30)
-                          <br />
-                        </p>
-                        <p className="depar">
-                          Arrival Time: {data.arrivalTime} (GMT+5:30)
-                          <br />
-                        </p>
-                        <p className="depar">
-                          Departure Time: {data.departureTime} (GMT+5:30)
-                          <br />
-                        </p>
+                      <div className="flightTextContainer">
+                        <div className="row">
+                          <div className="col-7">
+                            <h4 className="taxititlesec">
+                              {data.flightId}{" "}
+                              <span
+                                style={{
+                                  fontWeight: "normal",
+                                  fontSize: "14px",
+                                }}
+                              >
+                                {data.startAirport} - {data.destinationAirport}
+                              </span>
+                            </h4>
+                          </div>
+                          <div className="col-5" style={{ textAlign: "end" }}>
+                            {data.airline}
+                          </div>
+                        </div>
+                        {/* <br /> */}
+                        <div className="row">
+                          <div className="col-5">{data.departureDate}</div>
+                          <div className="col-5">{data.arrivalDate}</div>
+                          <div className="col-5">{data.departureTime}</div>
+                          <div className="col-5">{data.arrivalTime}</div>
+                          <div className="col-12" style={{ textAlign: "end" }}>
+                            <br />
+                          </div>
+                          <div className="col-6" style={{ textAlign: "end" }}>
+                            <h5>
+                              <span class="material-symbols-outlined">
+                                escalator_warning
+                              </span>
+                              {"  "}
+                              <b>Rs {data.economyClass}.00</b>
+                            </h5>
+                          </div>
+
+                          <div className="col-6" style={{ textAlign: "end" }}>
+                            <h5>
+                              <span class="material-symbols-outlined">
+                                airline_seat_recline_extra
+                              </span>
+                              {"  "}
+                              <b>Rs {data.businessClass}.00</b>
+                            </h5>
+                          </div>
+                        </div>
                       </div>
                     </div>
                   </Link>
