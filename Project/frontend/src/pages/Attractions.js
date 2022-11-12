a7bb0121b7c64eaee877eb89db8ad467b6274477
Sahan Perera <sahanvimukthi00@gmail.com>
2022-11-12 08:51:08 +0530

    did some changes

--- Project/frontend/src/pages/Attractions.js
+++ Project/frontend/src/pages/Attractions.js
@@ -1,16 +1,21 @@
-import '../styles/sahan/Attractions.css';
+import "../styles/sahan/Attractions.css";
 
 function Attractions() {
   return (
-    <div className='attractionsContainer'>
-      <div className='attImgContainer'>
-        <div className='attblurContainer'/>
+    <div className="attractionsContainer">
+      <div className="attImgContainer">
+        <div className="attblurContainer" />
         <h1>Attractions</h1>
         <h2>Find and book a great experience</h2>
-        <h3>Discover more of your destination and make the most of your trip</h3>
+        <h3>
+          Leave no highlight undiscovered as you experience the best things to
+          do in Sri Lanka. From top highlights to hidden gems, find the
+          unforgettable experiences in Sri Lanka. Discover more of your
+          destination and make the most of your trip.
+        </h3>
       </div>
     </div>
-  )
+  );
 }
 
 export default Attractions;
