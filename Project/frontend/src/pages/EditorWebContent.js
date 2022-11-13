fc7a5a9167288b1eb83cbe996dcc5c9252137c50
IT21008382 <it21008382@my.sliit.lk>
2022-11-13 10:46:35 +0530

    delete alert box added for vehicles in web content

--- Project/frontend/src/pages/EditorWebContent.js
+++ Project/frontend/src/pages/EditorWebContent.js
@@ -323,7 +323,7 @@ function EditorWebContent() {
         cold9hid = 'none';
         cold10hid = 'none';
         cold11 = <Link className='updatebttn' to={"/editorDashboard/vehicleUpdateForm/"+props._id}><span className="material-symbols-outlined">edit</span></Link>;
-        cold12 = <button className='deletebttn' onClick={() => deleteBooking(props._id)}><span className="material-symbols-outlined">delete</span></button>;
+        cold12 = <button className='deletebttn' onClick={() => {if (window.confirm('Do you really want to delete these record? This process cannot be undone.')) deleteBooking(props._id) }}><span className="material-symbols-outlined">delete</span></button>;
         addbttn = <Link to={'/editorDashboard/vehicleForm'}><button className='editorOverviewprintBtn'>Add</button></Link>
         break;
       case 'package':
